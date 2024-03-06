8bcb4d6d885ba4c57ae729fa4b926c03c4ad76f3
8XA <ochoa.ssa@gmail.com>
2024-03-05 18:20:47 -0700

    refactor: Custom injections for yaml queries for working with docassemble.

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -86,3 +86,133 @@
             (block_scalar) @injection.content
             (#set! injection.language "promql")
             (#offset! @injection.content 0 1 0 0))))))
+
+; Codigo python en los yaml de docassemble
+(block_mapping_pair
+  key: (flow_node) @_code
+  (#any-of? @_code "code" "script" "before_script" "after_script")
+  value:
+    (flow_node
+      (plain_scalar
+        (string_scalar) @injection.content)
+      (#set! injection.language "python")))
+
+(block_mapping_pair
+  key: (flow_node) @_code
+  (#any-of? @_code "code" "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_scalar) @injection.content
+      (#set! injection.language "python")
+      (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_code
+  (#any-of? @_code "code" "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content))
+          (#set! injection.language "python")))))
+
+(block_mapping_pair
+  key: (flow_node) @_code
+  (#any-of? @_code "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "python")
+            (#offset! @injection.content 0 1 0 0))))))
+
+; Codigo html en los yaml de docassemble para la tag css
+(block_mapping_pair
+  key: (flow_node) @_css
+  (#any-of? @_css "css" "script" "before_script" "after_script")
+  value:
+    (flow_node
+      (plain_scalar
+        (string_scalar) @injection.content)
+      (#set! injection.language "html")))
+
+(block_mapping_pair
+  key: (flow_node) @_css
+  (#any-of? @_css "css" "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_scalar) @injection.content
+      (#set! injection.language "html")
+      (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_css
+  (#any-of? @_css "css" "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content))
+          (#set! injection.language "html")))))
+
+(block_mapping_pair
+  key: (flow_node) @_css
+  (#any-of? @_css "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "html")
+            (#offset! @injection.content 0 1 0 0))))))
+
+; Codigo html en los yaml de docassemble para la tag script
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "script" "script" "before_script" "after_script")
+  value:
+    (flow_node
+      (plain_scalar
+        (string_scalar) @injection.content)
+      (#set! injection.language "html")))
+
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "script" "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_scalar) @injection.content
+      (#set! injection.language "html")
+      (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "script" "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content))
+          (#set! injection.language "html")))))
+
+(block_mapping_pair
+  key: (flow_node) @_script
+  (#any-of? @_script "script" "before_script" "after_script")
+  value:
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "html")
+            (#offset! @injection.content 0 1 0 0))))))
+
